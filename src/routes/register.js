1da4db8ebee9d7730e1cd80caa6104b6a1a81e91
marcosOP007 <97863306+marcosOP007@users.noreply.github.com>
2022-06-28 12:03:22 -0400

    update 0.0.1 28/06/2022

--- src/routes/register.js
+++ src/routes/register.js
@@ -5,7 +5,43 @@ const router = express.Router();
 const crypto = require('crypto');
 const jwt = require("jsonwebtoken");
 const SECRET = 'henmarnel';
+const pool = require('../dao/conexao');
 
+const validator = require('../private/js/validator.js')
+
+
+router.use(express.json());
+router.use(express.urlencoded({
+    extended: true
+}))
+
+
+
+
+
+
+
+function existUser(name){
+  
+  return pool.query(`SELECT *  FROM bd_ifchess WHERE name = '${name}'`)
+  .then(function(result){
+    if (result.rowCount == 0){
+      console.log("user not exist")
+      
+      return false;
+    }        
+
+    console.log("user exist");
+    return true;
+   
+ })
+ .catch(function(erro){
+   
+     console.log(erro.stack)
+     //res.render('lista');
+ })
+    
+}
 
 
 
@@ -26,9 +62,51 @@ router.get('/', (req, res, next ) => {
 
 
 router.post('/', (req, res, next ) => {
-    res.status(200).send({
-        mensagem: 'Usando o POST dentro da rota login'
-    })
+
+    
+  const validation_stats = validator.valid_info_resgister(req.body);
+
+ console.log("registrando usuario...")
+
+  
+  
+ if (validation_stats === false){
+  console.log("ERRO NO CADASTRO");
+  return res.status(201).send({
+    mensagem: "erro inesperado tente novamente" 
+  });
+}else{
+
+  existUser(req.body.username).then(function(rel){
+      if(rel){
+        return  res.status(200).send({
+          mensagem: 'Nome de usuario/email usado por outra pessoa'
+          })
+      }else{
+          
+    console.log("adicionando...")
+    pool.query(`INSERT INTO bd_ifchess VALUES('${req.body.username}', '${req.body.email}', '${crypto.createHash('sha256').update(req.body.password).digest('hex')}')`)
+
+    const token = jwt.sign({userId: 1},SECRET ,{ expiresIn: 300});
+
+    return res.json({ auth: true , token});
+
+      }
+   }).catch(function(){
+    return res.status(201).send({
+      mensagem: "houve um erro inesperado tente novamente :(" 
+    });
+   });
+
+ 
+  var a = existUser(req.body.username);
+  console.log("R  == "+existUser(req.body.username));
+ 
+  
+  //  return res.status(401).end();
+}
+
+  
 });
 
 
@@ -44,47 +122,3 @@ router.patch('/', (req, res, next) => {
 //
 
 module.exports = router;
-
-
-
-
-/*
-
-app.post('/login', (req,res) => {
-  /*
-  const response = {
-    first_name:req.body.first_name,
-    last_name:req.body.last_name  
-  }; 
-*/
-/*
-  const validation_stats = validator.valid_info_resgister(req.body);
-
- console.log("enviado com sucesso")
-
-  
-  if (validation_stats === false){
-    console.log("ERRO NO CADASTRO");
-    return res.status(201).send({
-      mensagem: "erro inesperado tente novamente" 
-    });
-  }else{
-
-    if(existUser(req.body.username)){
-
-    }else{
-      
-      pool.query(`INSERT INTO bd_ifchess VALUES('${req.body.username}', '${req.body.email}', '${crypto.createHash('sha256').update(req.body.password).digest('hex')}')`)
-
-      const token = jwt.sign({userId: 1},SECRET ,{ expiresIn: 300});
-
-      return res.json({ auth: true , token});
-
-    }
-  
-    
-      return res.status(401).end();
-  }
-
-    
-})*/
